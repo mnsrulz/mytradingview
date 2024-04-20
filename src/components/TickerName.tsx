b3d924a63610ac972a29dc54f1e0851957f2efd5
mnsrulz <manishrawat4u@gmail.com>
2024-04-20 21:26:03 +0000

    removed comment

--- src/components/TickerName.tsx
+++ src/components/TickerName.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { ITradeView } from "@/lib/types";
-import { shortDateFormatter } from "./trades";
 import dayjs from "dayjs";
 
 export const TickerName = (p: { trade: ITradeView; }) => {
     const { trade } = p;
-    debugger;
     const dt = `${trade.contractExpiry}`.substring(0, 10);  //keeping only the date part
     const isYearAfter = dayjs(dt).diff(dayjs(), 'days') > 365;
     const fmtDate = isYearAfter ? dayjs(dt).format('M/D/YY') : dayjs(dt).format('M/D');
