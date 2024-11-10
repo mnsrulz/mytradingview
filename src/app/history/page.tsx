ad55a81ebca901d0d007b2f64add238f783d3733
mnsrulz <manishrawat4u@gmail.com>
2024-11-10 14:39:22 -0500

    fix the history page build error

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,76 +1,16 @@
 'use client';
-import { useCachedReleaseData, useCachedReleaseSymbolData, useMyStockList } from '@/lib/socket';
-import { Dialog, DialogContent, DialogTitle, FormControl, FormControlLabel, Grid, ImageList, ImageListItem, InputLabel, LinearProgress, Link, MenuItem, Select, Switch, useMediaQuery, useTheme } from '@mui/material';
+import { useCachedReleaseData, useMyStockList } from '@/lib/socket';
+import { FormControl, FormControlLabel, Grid, InputLabel, LinearProgress, MenuItem, Select, Switch } from '@mui/material';
 import { useState } from 'react';
-import { IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { useMyLocalWatchList } from '@/lib/hooks';
-const D1 = (props: { dt: string, mytickersSymbols: string[], showAllSymbols: boolean }) => {
-    const { dt, mytickersSymbols, showAllSymbols } = props;
-    const { cachedSummarySymbolsData } = useCachedReleaseSymbolData(dt);
-    const theme = useTheme();
-    const matchesXs = useMediaQuery(theme.breakpoints.down("sm"));
-    const matchesMd = useMediaQuery(theme.breakpoints.down("md"));
-    const numberOfItemsToDisplay = matchesXs ? 2 : matchesMd ? 3 : 4;
-    const ts = cachedSummarySymbolsData.filter(r => showAllSymbols || mytickersSymbols.includes(r.name));    //make sure to load only those which are part of the watchlist.
+import { HistoricalDex } from '@/components/HistoricalDex';
 
-    const [openDialog, setOpenDialog] = useState(false);
-    const [selectedImage, setSelectedImage] = useState('');
-
-    const handleImageClick = (imageSrc: string) => {
-        setSelectedImage(imageSrc);
-        setOpenDialog(true);
-    };
-
-    const handleCloseDialog = () => {
-        setOpenDialog(false);
-    };
-    return <><ImageList cols={numberOfItemsToDisplay} gap={1}>
-        {ts.map((item) => (
-            <ImageListItem key={item.name} sx={{ width: '100%', height: '100px' }}>
-                <img src={`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`}
-                    style={{
-                        width: '100%', height: 'auto', objectFit: 'cover',
-                        transition: 'all 0.3s ease-in-out',
-                        cursor: 'pointer',
-                        // boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-                        // '&:hover': {
-                        //   boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
-                        //   transform: 'scale(1.05)',
-                        // }
-
-                    }}
-                    loading="lazy"
-                    onClick={() => handleImageClick(`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`)} />
-            </ImageListItem>
-        ))}
-    </ImageList>
-        <Dialog open={openDialog} onClose={handleCloseDialog} fullScreen={matchesXs}>
-            <DialogTitle>
-                <IconButton
-                    aria-label="close"
-                    onClick={handleCloseDialog}
-                    sx={{ position: 'absolute', right: 8, top: 8 }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent>
-                <img src={selectedImage} style={{ width: '100%', objectFit: 'contain' }} />
-            </DialogContent>
-        </Dialog>
-    </>;
-}
-
-export default function Page() {
-    const { mytickers, loading } = useMyStockList();
-    const { wl } = useMyLocalWatchList(mytickers);
+export default function Page() {    
     const [showAllSymbols, setShowAllSymbols] = useState(false);
     const { cachedSummaryData, isLoadingCachedSummaryData } = useCachedReleaseData();
     const [dataMode, setDataMode] = useState('');
-    if (isLoadingCachedSummaryData || loading) return <LinearProgress />;
+    if (isLoadingCachedSummaryData) return <LinearProgress />;
     const cachedDates = cachedSummaryData.map(j => j.name);
-    const mytickersSymbols = wl.map(r => r.symbol);
+
     const dt = dataMode || cachedDates.at(0) || '';
     return (
         <>
@@ -90,11 +30,11 @@ export default function Page() {
                 </Select>
             </FormControl>
             <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <FormControlLabel title='Show all symbols available for a given date or limit symbols available in your watchlist' control={<Switch checked={showAllSymbols}  onChange={(e, v) => setShowAllSymbols(v)} />} label="Show all?" />
+                <FormControlLabel title='Show all symbols available for a given date or limit symbols available in your watchlist' control={<Switch checked={showAllSymbols} onChange={(e, v) => setShowAllSymbols(v)} />} label="Show all?" />
             </FormControl>
             {/* <Link href='history/legacy'>Legacy Mode</Link> */}
             <Grid container>
-                <D1 dt={dt} mytickersSymbols={mytickersSymbols} showAllSymbols={showAllSymbols} ></D1>
+                <HistoricalDex dt={dt} showAllSymbols={showAllSymbols} />
             </Grid></>
     );
 }
---
+++ src/components/HistoricalDex.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { useCachedReleaseSymbolData } from '@/lib/socket';
+import { Dialog, DialogContent, DialogTitle, ImageList, ImageListItem, useMediaQuery, useTheme } from '@mui/material';
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { useMyLocalWatchList } from '@/lib/hooks';
+export const HistoricalDex = (props: { dt: string, showAllSymbols: boolean }) => {
+    const { wl } = useMyLocalWatchList([]);
+    const mytickersSymbols = wl.map(r => r.symbol);
+    const { dt, showAllSymbols } = props;
+    const { cachedSummarySymbolsData } = useCachedReleaseSymbolData(dt);
+    const theme = useTheme();
+    const matchesXs = useMediaQuery(theme.breakpoints.down("sm"));
+    const matchesMd = useMediaQuery(theme.breakpoints.down("md"));
+    const numberOfItemsToDisplay = matchesXs ? 2 : matchesMd ? 3 : 4;
+    const ts = cachedSummarySymbolsData.filter(r => showAllSymbols || mytickersSymbols.includes(r.name));    //make sure to load only those which are part of the watchlist.
+
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedImage, setSelectedImage] = useState('');
+
+    const handleImageClick = (imageSrc: string) => {
+        setSelectedImage(imageSrc);
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+    return <><ImageList cols={numberOfItemsToDisplay} gap={1}>
+        {ts.map((item) => (
+            <ImageListItem key={item.name} sx={{ width: '100%', height: '100px' }}>
+                <img src={`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`}
+                    style={{
+                        width: '100%', height: 'auto', objectFit: 'cover',
+                        transition: 'all 0.3s ease-in-out',
+                        cursor: 'pointer',
+                        // boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+                        // '&:hover': {
+                        //   boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
+                        //   transform: 'scale(1.05)',
+                        // }
+
+                    }}
+                    loading="lazy"
+                    onClick={() => handleImageClick(`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`)} />
+            </ImageListItem>
+        ))}
+    </ImageList>
+        <Dialog open={openDialog} onClose={handleCloseDialog} fullScreen={matchesXs}>
+            <DialogTitle>
+                <IconButton
+                    aria-label="close"
+                    onClick={handleCloseDialog}
+                    sx={{ position: 'absolute', right: 8, top: 8 }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>
+                <img src={selectedImage} style={{ width: '100%', objectFit: 'contain' }} />
+            </DialogContent>
+        </Dialog>
+    </>;
+}
