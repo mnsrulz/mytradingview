287c8130c176c7e5d14e58e5a6ca7011d9ad0914
mnsrulz <manishrawat4u@gmail.com>
2024-11-12 23:44:24 -0500

    improved version of slider in history page

--- src/components/HistoricalDex.tsx
+++ src/components/HistoricalDex.tsx
@@ -5,6 +5,9 @@ import { useState } from 'react';
 import { IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useMyLocalWatchList } from '@/lib/hooks';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+
 export const HistoricalDex = (props: { dt: string, showAllSymbols: boolean }) => {
     const { wl } = useMyLocalWatchList([]);
     const mytickersSymbols = wl.map(r => r.symbol);
@@ -14,6 +17,7 @@ export const HistoricalDex = (props: { dt: string, showAllSymbols: boolean }) =>
     const matchesXs = useMediaQuery(theme.breakpoints.down("sm"));
     const matchesMd = useMediaQuery(theme.breakpoints.down("md"));
     const numberOfItemsToDisplay = matchesXs ? 2 : matchesMd ? 3 : 4;
+    const imgWidth = `${(100/numberOfItemsToDisplay)}%`;
     const ts = cachedSummarySymbolsData.filter(r => showAllSymbols || mytickersSymbols.includes(r.name));    //make sure to load only those which are part of the watchlist.
 
     const [openDialog, setOpenDialog] = useState(false);
@@ -27,39 +31,52 @@ export const HistoricalDex = (props: { dt: string, showAllSymbols: boolean }) =>
     const handleCloseDialog = () => {
         setOpenDialog(false);
     };
-    return <><ImageList cols={numberOfItemsToDisplay} gap={1}>
-        {ts.map((item) => (
-            <ImageListItem key={item.name} sx={{ width: '100%', height: '100px' }}>
-                <img src={`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`}
-                    style={{
-                        width: '100%', height: 'auto', objectFit: 'cover',
-                        transition: 'all 0.3s ease-in-out',
-                        cursor: 'pointer',
-                        // boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-                        // '&:hover': {
-                        //   boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
-                        //   transform: 'scale(1.05)',
-                        // }
 
-                    }}
-                    loading="lazy"
-                    onClick={() => handleImageClick(`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`)} />
-            </ImageListItem>
+
+    return <PhotoProvider>
+    {/* <PhotoView src="/1.jpg">
+      <img src="/1-thumbnail.jpg" alt="" />
+    </PhotoView> */}
+    {ts.map((item) => (
+            <PhotoView key={item.assetUrl} src={item.assetUrl} >
+                <img src={item.assetUrl} width={imgWidth} style={{ objectFit: 'cover' }} alt={item.assetUrl}/>
+            </PhotoView>
         ))}
-    </ImageList>
-        <Dialog open={openDialog} onClose={handleCloseDialog} fullScreen={matchesXs}>
-            <DialogTitle>
-                <IconButton
-                    aria-label="close"
-                    onClick={handleCloseDialog}
-                    sx={{ position: 'absolute', right: 8, top: 8 }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent>
-                <img src={selectedImage} style={{ width: '100%', objectFit: 'contain' }} />
-            </DialogContent>
-        </Dialog>
-    </>;
+  </PhotoProvider>
+
+    // return <><ImageList cols={numberOfItemsToDisplay} gap={1}>
+    //     {ts.map((item) => (
+    //         <ImageListItem key={item.name} sx={{ width: '100%', height: '100px' }}>
+    //             <img src={`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`}
+    //                 style={{
+    //                     width: '100%', height: 'auto', objectFit: 'cover',
+    //                     transition: 'all 0.3s ease-in-out',
+    //                     cursor: 'pointer',
+    //                     // boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+    //                     // '&:hover': {
+    //                     //   boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
+    //                     //   transform: 'scale(1.05)',
+    //                     // }
+
+    //                 }}
+    //                 loading="lazy"
+    //                 onClick={() => handleImageClick(`https://mztrading-data.deno.dev/images?dt=${dt}&s=${item.name}`)} />
+    //         </ImageListItem>
+    //     ))}
+    // </ImageList>
+    //     <Dialog open={openDialog} onClose={handleCloseDialog} fullScreen={matchesXs}>
+    //         <DialogTitle>
+    //             <IconButton
+    //                 aria-label="close"
+    //                 onClick={handleCloseDialog}
+    //                 sx={{ position: 'absolute', right: 8, top: 8 }}
+    //             >
+    //                 <CloseIcon />
+    //             </IconButton>
+    //         </DialogTitle>
+    //         <DialogContent>
+    //             <img src={selectedImage} style={{ width: '100%', objectFit: 'contain' }} />
+    //         </DialogContent>
+    //     </Dialog>
+    // </>;
 }
