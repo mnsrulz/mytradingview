11138edf0f4d00fc72ccb0213842fe145ba5580f
mnsrulz <manishrawat4u@gmail.com>
2024-11-20 09:00:15 -0500

    no cache for mzdata

--- src/lib/mzDataService.ts
+++ src/lib/mzDataService.ts
@@ -4,6 +4,10 @@ export type CachedReleasesType = {
     name: string
 }
 
-export const getCachedSummaryData = ky(`https://mztrading-data.deno.dev/summary`).json<{ symbol: string, dt: string }[]>
+export const getCachedSummaryData = ky(`https://mztrading-data.deno.dev/summary`, {
+    cache: "no-store"   //no cache for this data
+}).json<{ symbol: string, dt: string }[]>
 
-export const getCachedReleaseData = ky(`https://mztrading-data.deno.dev/releases`).json<CachedReleasesType[]>
+export const getCachedReleaseData = ky(`https://mztrading-data.deno.dev/releases`, {
+    cache: "no-store"   //no cache for this data
+}).json<CachedReleasesType[]>
