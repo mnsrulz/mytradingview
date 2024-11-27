dc4493541650a5a0b484f15a5efdaa80b1b427ba
mnsrulz <manishrawat4u@gmail.com>
2024-11-27 14:43:34 -0500

    showing suspense for now

--- src/app/options/analyze/[symbol]/page.tsx
+++ src/app/options/analyze/[symbol]/page.tsx
@@ -1,17 +1,25 @@
 import { getDexGexAnalysisView } from "@/lib/tradierService";
 import { C } from "./C";
 import { DexGexType } from "@/lib/types";
+import { Suspense } from "react";
+import { LinearProgress } from "@mui/material";
+
+const ChartWrapper = async (props: { symbol: string, dte: number, tab: DexGexType, strikeCountValue: number }) => {
+    const { symbol, dte, tab, strikeCountValue } = props;
+    const cachedDates: string[] = [];//await getCachedSummaryDatesBySymbol(symbol);
+    const { currentPrice, mappedOptions } = await getDexGexAnalysisView(symbol);
+    const dataMode = 'Live'
+    return <C cachedDates={cachedDates} dte={dte} sc={strikeCountValue} dataMode={dataMode} data={mappedOptions} symbol={symbol} price={currentPrice} tab={tab} />
+}
+
 export default async function Page({ params, searchParams }: { params: { symbol: string }, searchParams: { [key: string]: string | number } }) {
     const search = searchParams;
     const { symbol } = params;
     const strikeCountValue = (search['sc'] || 30) as number;
     const dte = (search['dte'] || 50) as number;
     const tab = (search['tab'] || 'DEX') as DexGexType;
-    const dataMode = 'Live'
 
-    const cachedDates: string[] = [];//await getCachedSummaryDatesBySymbol(symbol);
-    
-    const { currentPrice, mappedOptions } = await getDexGexAnalysisView(symbol);
-
-    return <C cachedDates={cachedDates} dte={dte} sc={strikeCountValue} dataMode={dataMode} data={mappedOptions} symbol={symbol} price={currentPrice} tab={tab} />
+    return <Suspense fallback={<LinearProgress />}>
+        <ChartWrapper symbol={symbol} dte={dte} strikeCountValue={strikeCountValue} tab={tab} />
+    </Suspense>
 }
