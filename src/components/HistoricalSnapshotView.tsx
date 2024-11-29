c5de4dcb1bd83ea7c5d835de287d8cdb85d1f052
mnsrulz <manishrawat4u@gmail.com>
2024-11-29 01:04:33 -0500

    build fix

--- src/components/HistoricalSnapshotView.tsx
+++ src/components/HistoricalSnapshotView.tsx
@@ -1,13 +1,7 @@
 'use client';
-import { Grid, ImageList, LinearProgress, useMediaQuery, useTheme } from '@mui/material';
+import { Grid, LinearProgress, useMediaQuery, useTheme } from '@mui/material';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
-import { forwardRef } from 'react';
-const Image = forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(
-    ({ src, ...props }, ref) => {
-        return <img ref={ref} src={src} className="mr-2 mb-2 w-24 h-24 cursor-pointer object-cover" alt="" {...props} />;
-    },
-);
 
 export const HistoricalSnapshotView = (props: { isLoading: boolean, items: { key: string, assetUrl: string }[] }) => {
     const { isLoading, items } = props;
@@ -21,8 +15,7 @@ export const HistoricalSnapshotView = (props: { isLoading: boolean, items: { key
         <PhotoProvider>
             {items.map((item) => (
                 <PhotoView key={item.key} src={item.assetUrl} >
-                    <Image key={item.key} loading='lazy' src={item.assetUrl} width={imgWidth} height="auto" style={{ objectFit: 'cover' }} alt={item.assetUrl} />
-                    {/* <img key={item.key} loading='lazy' src={item.assetUrl} width={imgWidth} height="auto" style={{ objectFit: 'cover' }} alt={item.assetUrl} /> */}
+                    <img key={item.key} loading='lazy' src={item.assetUrl} width={imgWidth} height="auto" style={{ objectFit: 'cover' }} alt={item.assetUrl} />
                 </PhotoView>
             ))}
         </PhotoProvider>
