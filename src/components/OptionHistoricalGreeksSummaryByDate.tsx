ce6cc7ae397ce42fc545afc9f965705f6c7e1f9a
mnsrulz <manishrawat4u@gmail.com>
2025-03-05 23:45:56 -0500

    minor enhancements in the UI for greeks report

--- src/components/OptionHistoricalGreeksSummaryByDate.tsx
+++ src/components/OptionHistoricalGreeksSummaryByDate.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { Stack, Box } from '@mui/material';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridDensity, GridToolbar } from '@mui/x-data-grid';
 import { getHistoricalGreeksSummaryByDate } from '@/lib/mzDataService';
 import { useQueryState, parseAsStringLiteral } from 'nuqs';
 import { useEffect, useState } from 'react';
@@ -11,43 +11,43 @@ import { OptionGreeksSummaryByDateResponse } from '@/lib/types';
 
 const columns: GridColDef<OptionGreeksSummaryByDateResponse>[] = [
     {
-        field: 'option_symbol', headerName: 'Symbol', flex: 1
+        field: 'option_symbol', headerName: 'Symbol'
     },
     {
-        field: 'call_delta', headerName: 'CALL Delta', type: 'number', flex: 1
+        field: 'call_delta', headerName: 'CALL Delta', type: 'number'
     },
     {
-        field: 'put_delta', headerName: 'PUT Delta', type: 'number', flex: 1
+        field: 'put_delta', headerName: 'PUT Delta', type: 'number'
     },
     {
-        field: 'call_put_dex_ratio', headerName: 'CALL/PUT DEX', type: 'number', flex: 1
+        field: 'call_put_dex_ratio', headerName: 'CALL/PUT DEX', type: 'number'
     },
     {
-        field: 'call_gamma', headerName: 'CALL y', type: 'number', flex: 1
+        field: 'call_gamma', headerName: 'CALL y', type: 'number'
     },
     {
-        field: 'put_gamma', headerName: 'PUT y', type: 'number', flex: 1
+        field: 'put_gamma', headerName: 'PUT y', type: 'number'
     },
     {
-        field: 'net_gamma', headerName: 'NET y', type: 'number', flex: 1
+        field: 'net_gamma', headerName: 'NET y', type: 'number'
     },
     {
-        field: 'call_volume', headerName: 'Call Volume', type: 'number', flex: 1
+        field: 'call_volume', headerName: 'Call Volume', type: 'number'
     },
     {
-        field: 'put_volume', headerName: 'Put Volume', type: 'number', flex: 1
+        field: 'put_volume', headerName: 'Put Volume', type: 'number'
     },
     {
-        field: 'call_put_volume_ratio', headerName: 'CALL/PUT Volume', type: 'number', flex: 1
+        field: 'call_put_volume_ratio', headerName: 'CALL/PUT Volume', type: 'number'
     },
     {
-        field: 'call_oi', headerName: 'Call OI', type: 'number', flex: 1
+        field: 'call_oi', headerName: 'Call OI', type: 'number'
     },
     {
-        field: 'put_oi', headerName: 'Put OI', type: 'number', flex: 1
+        field: 'put_oi', headerName: 'Put OI', type: 'number'
     },
     {
-        field: 'call_put_oi_ratio', headerName: 'CALL/PUT OI', type: 'number', flex: 1
+        field: 'call_put_oi_ratio', headerName: 'CALL/PUT OI', type: 'number'
     }
 ];
 
@@ -55,9 +55,10 @@ export const OptionHistoricalGreeksSummaryByDate = (props: { cachedDates: string
     const { cachedDates } = props;
     const [date, setDate] = useQueryState('dt', parseAsStringLiteral(cachedDates).withDefault(cachedDates.at(0) || ''));
     const [rows, setRows] = useState<OptionGreeksSummaryByDateResponse[]>([]);
-    const [hasLoaded, setHasLoaded] = useState<boolean>(false);
+    const [hasLoaded, setHasLoaded] = useState(false);
     const [minVolume, setMinVolume] = useState(1000);
     const [minOpenInterest, setMinOpenInterest] = useState(10000);
+    const [gridDensity, setGridDensity] = useState<GridDensity>('compact');
     const filteredData = rows.filter(k => {
         if (minVolume && (k.call_volume + k.put_volume < minVolume)) return false;
         if (minOpenInterest && (k.call_oi + k.put_oi < minOpenInterest)) return false;
@@ -66,7 +67,7 @@ export const OptionHistoricalGreeksSummaryByDate = (props: { cachedDates: string
     useEffect(() => {
         setHasLoaded(false)
         getHistoricalGreeksSummaryByDate(date).then(d => {
-            debugger;
+            // debugger;
             setRows(d);
             setHasLoaded(true)
         })
@@ -118,14 +119,27 @@ export const OptionHistoricalGreeksSummaryByDate = (props: { cachedDates: string
                     },
                 },
             }}
-            density='compact'
             loading={!hasLoaded}
             getRowId={(r: OptionGreeksSummaryByDateResponse) => r.option_symbol}
             // pageSizeOptions={[5]}
             checkboxSelection={false}
             disableColumnFilter
             disableRowSelectionOnClick
+            onDensityChange={setGridDensity}
             disableColumnMenu={true}
+            density={gridDensity}
+            sx={{
+                '.MuiDataGrid-columnSeparator': {
+                    display: 'none'
+                },
+                fontFamily: 'sans-serif'
+            }}
+            slots={{ toolbar: GridToolbar }}
+            slotProps={{
+                toolbar: {
+                    showQuickFilter: true,
+                },
+            }}
         />
 
     </Box>
