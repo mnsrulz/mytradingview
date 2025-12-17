bbf660d0ede8196ee64cb5fa90fd7387d1e5d60e
mnsrulz <manishrawat4u@gmail.com>
2025-12-16 23:29:14 -0500

    sort the symbols

--- src/app/options/iv/page.tsx
+++ src/app/options/iv/page.tsx
@@ -3,6 +3,6 @@ import { Wrapper } from "@/components/IVHistorical/Wrapper";
 
 export default async function Page() {
     const watchList = await getWatchlist();
-    const symbols = watchList.map(k=> k.symbol);
+    const symbols = watchList.map(k=> k.symbol).sort();
     return <Wrapper symbols={symbols} />
 }
